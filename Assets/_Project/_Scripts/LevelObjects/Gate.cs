466fc8acef202e0fcc9e34d718585dca6ab33bf6
Plutzz <benjiongco@gmail.com>
2024-02-13 09:42:05 -0800

    Add Gate Level Object

---
+++ Assets/_Project/_Scripts/LevelObjects/Gate.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private StageButton button;
+    private Animator anim;
+    private string currentState;
+    private Collider2D gateCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        gateCollider = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        button.onButtonDown += OpenGate;
+        button.onButtonUp += CloseGate;
+    }
+
+    private void OpenGate()
+    {
+        ChangeAnimationState("GateOpen");
+        gateCollider.enabled = false;
+    }
+
+    private void CloseGate()
+    {
+        ChangeAnimationState("GateClose");
+        gateCollider.enabled = true;
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+
+        // Ensures state change is only triggered once
+        if (currentState == newState) return;
+
+        anim.Play(newState);
+
+        // reassign the current state
+        currentState = newState;
+    }
+
+}
