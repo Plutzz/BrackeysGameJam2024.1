d37ba897f4d0811a11584161b1eba1dda760f7e2
Plutzz <benjiongco@gmail.com>
2024-02-13 13:30:55 -0800

    Add hit stop to player health

--- Assets/_Project/_Scripts/PlayerHealth.cs
+++ Assets/_Project/_Scripts/PlayerHealth.cs
@@ -6,16 +6,42 @@ public class PlayerHealth : UnitHealth
 {
     [SerializeField] private float knockUpAmount;
 
+    [Header("Invincibility")]
     [SerializeField] private float invincibilityTime;
     [SerializeField] private int invincibilityBlinks;
     [SerializeField] private SpriteRenderer graphics;
-
     private bool canTakeDamage;
+
+    [Header("TimeStop")]
+    [SerializeField] private float hitStopTimeScale = 0.05f;        //How slow game slows down to when hit
+    [SerializeField] private float hitStopRestoreSpeed = 10f;       //Speed at which time scale is restored
+    [SerializeField] private float hitStopDelay = 0.1f;             //How much time before time scales begins to restore
+    private float timeRestoreSpeed;
+    private bool restoreTime;
+
+
 
     protected override void Awake()
     {
         base.Awake();
         canTakeDamage = true;
+    }
+
+    private void Update()
+    {
+        if (restoreTime)
+        {
+            if (Time.timeScale < 1f)
+            {
+                Time.timeScale += Time.deltaTime * timeRestoreSpeed;
+            }
+            else
+            {
+                //playerMovement.enabled = true;
+                Time.timeScale = 1f;
+                restoreTime = false;
+            }
+        }
     }
 
     public override void TakeDamage(float damage)
@@ -24,6 +50,11 @@ public class PlayerHealth : UnitHealth
         if (!canTakeDamage) return;
 
         base.TakeDamage(damage);
+
+        if(health > 0)
+        {
+            StopTime(hitStopTimeScale, hitStopRestoreSpeed, hitStopDelay);
+        }
 
         StartCoroutine(Invincibility());
     }
@@ -59,5 +90,29 @@ public class PlayerHealth : UnitHealth
         }
 
         canTakeDamage = true;
+    }
+
+    private void StopTime(float _changeTimeScale, float _restoreSpeed, float _delay)
+    {
+        timeRestoreSpeed = _restoreSpeed;
+
+        if (_delay > 0)
+        {
+            //playerMovement.enabled = false;
+            StopCoroutine(StartTime(_delay));
+            StartCoroutine(StartTime(_delay));
+        }
+        else
+        {
+            restoreTime = true;
+        }
+
+        Time.timeScale = _changeTimeScale;
+    }
+
+    private IEnumerator StartTime(float amt)
+    {
+        yield return new WaitForSecondsRealtime(amt);
+        restoreTime = true;
     }
 }
