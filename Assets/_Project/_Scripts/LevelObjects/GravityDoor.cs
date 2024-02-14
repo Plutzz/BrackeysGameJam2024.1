052cd26d1c37ab446f0d1f2ede49bafcf33e78ac
Plutzz <benjiongco@gmail.com>
2024-02-13 17:11:11 -0800

    Create Box and GravityDoor

---
+++ Assets/_Project/_Scripts/LevelObjects/GravityDoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityDoor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerMovement>().FlipGravity();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+    }
+
+}
