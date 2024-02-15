def6f79b65767583e350b667c32f0ba125750ac1
Plutzz <benjiongco@gmail.com>
2024-02-15 13:36:30 -0800

    Add Sibling Rule Tiles

---
+++ Assets/_Project/_Scripts/SiblingRuleTile.cs
@@ -0,0 +1,36 @@
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SiblingRuleTile : RuleTile
+{
+
+    public enum SiblingGroup
+    {
+        Grass,
+        Tech,
+    }
+    public SiblingGroup siblingGroup;
+
+    public override bool RuleMatch(int neighbor, TileBase other)
+    {
+        if (other is RuleOverrideTile)
+            other = (other as RuleOverrideTile).m_InstanceTile;
+
+        switch (neighbor)
+        {
+            case TilingRule.Neighbor.This:
+                {
+                    return other is SiblingRuleTile
+                        && (other as SiblingRuleTile).siblingGroup == this.siblingGroup;
+                }
+            case TilingRule.Neighbor.NotThis:
+                {
+                    return !(other is SiblingRuleTile
+                        && (other as SiblingRuleTile).siblingGroup == this.siblingGroup);
+                }
+        }
+
+        return base.RuleMatch(neighbor, other);
+    }
+}
