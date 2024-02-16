9175c3dca323936363b304e3d297fc66691bc3c0
Ratske99 <sethlaske9999@gmail.com>
2024-02-15 23:32:24 -0800

    Added level data reset to start screen

--- Assets/_Project/_Scripts/MainMenu.cs
+++ Assets/_Project/_Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,11 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
 
+    [SerializeField] private PresetSpawnPositions[] spawnPositions;
+
     public void StartGame()
     {
+        ResetSOs();
         SceneManager.LoadScene(1);
     }
 
@@ -22,4 +26,18 @@ public class MainMenu : MonoBehaviour
         settingsMenu.SetActive(true);
         gameObject.SetActive(false);
     }
+
+    private void ResetSOs() {
+        foreach (var SO in spawnPositions) {
+            SO.levelData.respawnLocation = SO.originalSpawnpoint;
+            SO.levelData.cameraIndex = SO.originalCamera;
+        }
+    }
+}
+
+[Serializable]
+public class PresetSpawnPositions {
+    public LevelDataScriptableObject levelData;
+    public Vector3 originalSpawnpoint;
+    public int originalCamera;
 }
